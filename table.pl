984e2e1e51016386a5322ce5dd6b69e81873cad9
Liel Avraham <liel.av07@gmail.com>
2026-03-28 12:40:52 +0300

    5

---
+++ table.pl
@@ -0,0 +1,21 @@
+and(A, B) :- A, B.
+
+or(A, B) :- A | B.
+
+xor(A, B) :- and(not(A), B) | and(A, not(B)).
+
+equal(A, B) :- A, B | not(A), not(B).
+
+nor(A, B) :- not(A), not(B).
+
+nand(A, B) :- not(and(A, B)).
+
+eval(X, S) :- X, !, S='true'.
+eval(X, S) :- not(X), !, S='fail'.
+
+table(A, B, X) :- write('true  true  '), A=true, B=true, eval(X, S), writeln(S), fail.
+table(A, B, X) :- write('true  fail  '), A=true, B=fail, eval(X, S), writeln(S), fail.
+table(A, B, X) :- write('fail  true  '), A=fail, B=true, eval(X, S), writeln(S), fail.
+table(A, B, X) :- write('fail  fail  '), A=fail, B=fail, eval(X, S), writeln(S), fail.
+
+table(_, _, _).
